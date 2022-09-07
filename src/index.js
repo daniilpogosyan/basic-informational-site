0a19921dd659acb0552f53a5b8d9a86ffbae7101
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-09-08 04:46:06 +0500

    Send html pages from server depending on url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs/promises');
 
 const hostname = '127.0.0.1';
 const port = '8080';
@@ -6,7 +7,33 @@ const port = '8080';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/html');
-  res.end('hello wrld?');
+
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  let htmlFilename;
+  switch (url.pathname) {
+    case '/': {
+      htmlFilename = 'index.html';
+      break;
+    }
+    case '/about': {
+      htmlFilename = 'about.html';
+      break;
+    }
+    case '/contact-me': {
+      htmlFilename = 'contact-me.html';
+      break;
+    }
+    default: {
+      htmlFilename = '404.html';
+      break;
+    }
+  }
+
+  fs.readFile(__dirname + '/pages/' + htmlFilename, 'utf8')
+  .then(html => {
+    res.end(html);
+  });
 });
 
 server.listen(port, hostname, () => {
